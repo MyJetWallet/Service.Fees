2b7a09429232a7fb8e5395ce55ef1b01667a4a23
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-07-22 00:15:09 +0300

    -test app;

--- test/TestApp/Program.cs
+++ test/TestApp/Program.cs
@@ -1,76 +1,88 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using MyNoSqlServer.DataReader;
+using MyNoSqlServer.DataWriter;
 using Newtonsoft.Json;
 using ProtoBuf.Grpc.Client;
-using Service.Fees.Client.Grpc;
-using Service.Fees.Domain.Models;
-using Service.Fees.Grpc.Models;
+using Service.Fees.MyNoSql;
 
 namespace TestApp
 {
     class Program
     {
+
+        public static string GetUrl()
+        {
+            // return "http://192.168.1.80:5123";
+            return "192.168.1.80:5125";
+        }
+
+        public static async Task ReadAssetsFees()
+        {
+            // var myNoSqlClient = new MyNoSqlTcpClient( GetUrl,"local");
+            // myNoSqlClient.Start();
+            // Thread.Sleep(10000);
+            
+            // var subs = new MyNoSqlReadRepository<AssetFeesNoSqlEntity>(myNoSqlClient,
+            //     AssetFeesNoSqlEntity.TableName);
+
+            // var client = new AssetFeesClient(subs);
+            // var fees = client.GetAssetFees("jetwallet", "BTC", OperationType.Withdrawal);
+            //
+            // Console.WriteLine(JsonConvert.SerializeObject(fees));
+            //
+            // var entities1 = subs.Get();
+            // Console.WriteLine(JsonConvert.SerializeObject(entities1));
+
+            
+            var writer = new MyNoSqlServerDataWriter<AssetFeesNoSqlEntity>(GetUrl, AssetFeesNoSqlEntity.TableName, false);
+            var entities = await writer.GetAsync();
+            Console.WriteLine(JsonConvert.SerializeObject(entities));
+        }
+
+        public async static Task ReadInstrumentsFees()
+        {
+            //http://192.168.1.80:5123
+            // var writer = new MyNoSqlServerDataWriter<SpotInstrumentFeesNoSqlEntity>(GetUrl, SpotInstrumentFeesNoSqlEntity.TableName, false);
+            // var entities = await writer.GetAsync();
+            // Console.WriteLine(JsonConvert.SerializeObject(entities));
+            
+            //192.168.1.80:5125
+            var myNoSqlClient = new MyNoSqlTcpClient( GetUrl,"local");
+            var subs = new MyNoSqlReadRepository<SpotInstrumentFeesNoSqlEntity>(myNoSqlClient,
+                SpotInstrumentFeesNoSqlEntity.TableName);
+            myNoSqlClient.Start();
+            Thread.Sleep(10000);
+            
+            var entities = subs.Get();
+            
+            Console.WriteLine(JsonConvert.SerializeObject(entities));
+            //
+            // var entity = subs.Get(SpotInstrumentFeesNoSqlEntity.GeneratePartitionKey("jetwallet"),
+            //     SpotInstrumentFeesNoSqlEntity.GenerateRowKey("ALGOUSD"));
+            //
+            // Console.WriteLine(JsonConvert.SerializeObject(entity));
+            //
+            // var entities = subs.Get(SpotInstrumentFeesNoSqlEntity.GeneratePartitionKey("jetwallet"));
+            //
+            // Console.WriteLine(JsonConvert.SerializeObject(entities));
+            //
+            // var entities1 = subs.Get();
+            //
+            // Console.WriteLine(JsonConvert.SerializeObject(entities1));
+        }
+        
         static async Task Main(string[] args)
         {
             GrpcClientFactory.AllowUnencryptedHttp2 = true;
 
             Console.Write("Press enter to start");
             Console.ReadLine();
-
-            var factory = new FeesClientFactory("http://localhost:88");
-
-            var settingsClient = factory.GetFeesSettingsService();
-
-            await settingsClient.SetFeesSettingsAsync(new FeesSettings()
-            {
-                BrokerId = "test",
-                AccountId = "OrangeDevAccount",
-                WalletId = "OrangeDevWallet"
-            });
-
-            var settings = await settingsClient.GetFeesSettingsAsync(new GetFeesSettingsRequest()
-                {
-                    BrokerId = "test"
-                }
-            );
-
-            Console.WriteLine(JsonConvert.SerializeObject(settings));
-
-            var assetFeesClient = factory.GetAssetFeesService();
-            await assetFeesClient.SetAssetFeesAsync(new AssetFees()
-            {
-                BrokerId = "test",
-                AssetId = "BTC",
-                FeeType = FeeType.ClientFee,
-                OperationType = OperationType.Withdrawal,
-                FeeSizeType = FeeSizeType.Absolute,
-                FeeSize = 0.0001
-            });
-
-            var assetFees = await assetFeesClient.GetAssetFees(new GetAssetFeesRequest()
-                {
-                    BrokerId = "test", AssetId = "BTC", OperationType = OperationType.Withdrawal
-                }
-            );
-
-            Console.WriteLine(JsonConvert.SerializeObject(assetFees));
-
-            var instrumentClient = factory.GetSpotInstrumentFeesService();
-            await instrumentClient.SetSpotInstrumentFeesAsync(new SpotInstrumentFees()
-            {
-                BrokerId = "test",
-                SpotInstrumentId = "BTCUSD",
-                FeeType = FeeType.ClientFee,
-                MakerFeeSizeType = FeeSizeType.Percentage,
-                MakerFeeSize = 0.0001,
-                TakerFeeSizeType = FeeSizeType.Percentage,
-                TakerFeeSize = 0.0002
-            });
-
-            var instrumentFees = await instrumentClient.GetSpotInstrumentFeesAsync(new GetSpotInstrumentFeesRequest()
-                {BrokerId = "test", SpotInstrumentId = "BTCUSD"});
-
-            Console.WriteLine(JsonConvert.SerializeObject(instrumentFees));
+            
+            // await ReadAssetsFees();
+            await ReadInstrumentsFees();
+            
 
             Console.WriteLine("End");
             Console.ReadLine();
