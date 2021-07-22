94c36454fc0bbc6f05f5ceecbb74aa257f2da87c
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-07-22 10:54:46 +0300

    - add autofac;

---
+++ src/Service.Fees.Client/FeesDictionaryAutofacHelper.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using MyNoSqlServer.DataReader;
+using Service.Fees.MyNoSql;
+
+namespace Service.Fees.Client
+{
+    public static class FeesDictionaryAutofacHelper
+    {
+        /// <summary>
+        /// Register interface:
+        ///   * IFeesSettingsClient
+        /// </summary>
+        public static void RegisterFeesClients(this ContainerBuilder builder,
+            IMyNoSqlSubscriber myNoSqlSubscriber)
+        {
+            var feesSettings = new MyNoSqlReadRepository<FeesSettingsNoSqlEntity>(myNoSqlSubscriber,
+                FeesSettingsNoSqlEntity.TableName);
+            
+            var assetFees = new MyNoSqlReadRepository<AssetFeesNoSqlEntity>(myNoSqlSubscriber,
+                AssetFeesNoSqlEntity.TableName);
+            
+            var instrumentFees = new MyNoSqlReadRepository<SpotInstrumentFeesNoSqlEntity>(myNoSqlSubscriber,
+                SpotInstrumentFeesNoSqlEntity.TableName);
+
+            builder
+                .RegisterInstance(new AssetFeesClient(assetFees, feesSettings))
+                .As<IAssetFeesClient>()
+                .AutoActivate()
+                .SingleInstance();
+
+            builder
+                .RegisterInstance(new SpotInstrumentFeesClient(instrumentFees, feesSettings))
+                .As<ISpotInstrumentFeesClient>()
+                .AutoActivate()
+                .SingleInstance();
+        }
+    }
+}
