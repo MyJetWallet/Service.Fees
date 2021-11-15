1ec3bb1eb85a229245b900fd79faaa269b6dac71
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-11-15 13:40:25 +0200

    net 6 migration

--- src/Service.Fees/Startup.cs
+++ src/Service.Fees/Startup.cs
@@ -30,11 +30,7 @@ namespace Service.Fees
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCodeFirstGrpc(options =>
-            {
-                options.Interceptors.Add<PrometheusMetricsInterceptor>();
-                options.BindMetricsInterceptors();
-            });
+            services.BindCodeFirstGrpc();
 
             services.AddHostedService<ApplicationLifetimeManager>();
 
